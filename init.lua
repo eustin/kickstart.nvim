17789d2ae151fab2a20e344a7707ff3d259f5c4d
eustin <eustin.jvans@gmail.com>
2025-04-13 09:55:53 +1000

    change java langauge server to jdtls

--- init.lua
+++ init.lua
@@ -572,17 +572,9 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        -- gopls = {},
         pyright = {},
-        java_language_server = {},
+        jdtls = {},
         jsonls = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
         --
 
