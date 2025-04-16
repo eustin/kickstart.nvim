dd7134407b41efbe3cb0a98b29ebb2e9bb310dcb
eustin <eustin.jvans@gmail.com>
2025-04-17 07:28:45 +1000

    add heading before fugitive remaps

--- init.lua
+++ init.lua
@@ -113,6 +113,7 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Git fugitive remaps
 vim.keymap.set('n', '<leader>g', ':Git<CR>', { desc = 'Git status' })
 vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', { desc = 'Git blame in vertical split' })
 vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', { desc = 'Git blame in vertical split' })
