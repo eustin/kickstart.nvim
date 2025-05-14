5b4e7b030399edbe3f08118a4efa4257770d3397
eustin <eustin.jvans@gmail.com>
2025-05-15 08:51:11 +1000

    enable smart indenting

--- init.lua
+++ init.lua
@@ -9,6 +9,9 @@ vim.opt_global.shiftwidth = 4
 vim.opt_global.expandtab = true
 vim.opt_global.autoindent = true
 
+-- Indentation
+vim.opt.smartindent = true
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
