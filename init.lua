de547d9452d1c37eef830537459f2edc2968a78e
eustin <eustin.jvans@gmail.com>
2025-06-19 08:03:27 +1000

    Add keymap to show all registers

--- init.lua
+++ init.lua
@@ -225,6 +225,7 @@ require('lazy').setup {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>r', builtin.registers, { desc = 'Show [R]egisters' })
 
       vim.keymap.set('n', '<leader>/', function()
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
