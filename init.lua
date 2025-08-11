76b7d046fac566e57c87ac2fbe90fe4e965c9869
eustin <eustin.jvans@gmail.com>
2025-08-12 08:19:17 +1000

    add debug adapter protocol plugins

--- init.lua
+++ init.lua
@@ -590,6 +590,22 @@ require('lazy').setup {
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 
+  -- debugging
+  -- 
+  -- see: https://github.com/NeuralNine/config-files/blob/master/.config/nvim/lua/plugins/nvim-dap.lua
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'rcarriga/nvim-dap-ui',
+      'theHamsta/nvim-dap-virtual-text',
+
+      -- language-specific implementations
+      'mfussenegger/nvim-dap-python',
+    }
+
+
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
